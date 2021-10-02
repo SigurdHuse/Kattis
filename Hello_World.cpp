545cb90e82fa7562c0c4bf573e71aed19377ef33
Sigurdkh <sigurdkh1234@gmail.com>
2021-10-02 17:10:45 +0200

    Added problem description

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,6 +1,17 @@
 #include <iostream> 
 using namespace std; 
 
+/*
+
+Input
+
+There is no input for this problem.
+Output
+
+Output should contain one line, containing the string “Hello World!”.
+
+*/
+
 int main()
 {
     cout << "Hello World!";
